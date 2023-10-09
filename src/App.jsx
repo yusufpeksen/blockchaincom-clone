05db3fcb31c3018062f4c036cdea107944ae4895
Ahmed Yusuf Pekşen <ahmetpeksen77@gmail.com>
2023-10-09 22:06:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import WarningOnTopPage from "../src/layouts/WarningOnTopPage/index"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+      <WarningOnTopPage />
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/layouts/WarningOnTopPage/index.jsx
@@ -0,0 +1,7 @@
+export default function WarningOnTopPage() {
+    return (
+        <a className="py-3 px-4 flex fixed justify-center items-center font-medium w-full text-white text-[16px] z-10 bg-[linear-gradient(to_right,rgb(15,24,44),rgb(11,51,82),rgb(18,103,175),rgb(23,89,218),rgb(18,103,175),rgb(11,51,82),rgb(15,24,44))] leading-[1.15] hover:bg-[linear-gradient(to_right,rgb(7,13,23),rgb(8,50,82),rgb(11,48,112),rgb(8,50,82),rgb(7,13,23))]">
+            Secure your crypto with Blockchain.com. Learn more.
+        </a>
+    )
+}
